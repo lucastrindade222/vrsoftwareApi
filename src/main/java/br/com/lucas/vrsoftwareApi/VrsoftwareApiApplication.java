1d708418c115794ac26332a57e93e5d3e0ce60bf
lucas <lucas500000000@gmail.com>
2022-09-30 12:32:25 -0300

    New Projeto

---
+++ src/main/java/br/com/lucas/vrsoftwareApi/VrsoftwareApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vrsoftwareApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrsoftwareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrsoftwareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/vrsoftwareApi/VrsoftwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vrsoftwareApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrsoftwareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
