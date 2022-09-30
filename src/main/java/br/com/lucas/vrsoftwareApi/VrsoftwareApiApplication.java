fc3b93d206870717293e789b213d5fde6c7af3a4
lucas <lucas500000000@gmail.com>
2022-09-30 15:46:08 -0300

    Initial setting
     1.Creation of dockerFile.
     2.Connection with postgresql database.
    3.Creation of the project's active profiles.

--- src/main/java/br/com/lucas/vrsoftwareApi/VrsoftwareApiApplication.java
+++ src/main/java/br/com/lucas/vrsoftwareApi/VrsoftwareApiApplication.java
@@ -8,6 +8,7 @@ public class VrsoftwareApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VrsoftwareApiApplication.class, args);
+		System.out.println(" α O elogio que vem daquele que merece o elogio está acima de todas as recompensas.'Faramir - O Senhor dos Anéis' ω");
 	}
 
 }
